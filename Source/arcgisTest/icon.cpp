88d1bcecd19d00dfd9732f51e79b4e0103d6a53e
staroblack <andrew.su1209@gmail.com>
2024-11-12 11:26:09 +0800

    com

--- Source/arcgisTest/icon.cpp
+++ Source/arcgisTest/icon.cpp
@@ -79,14 +79,14 @@ void Aicon::complete()
 	meshComponent->OnComponentCreated();
 	meshComponent->RegisterComponent();
 	meshComponent->SetWorldLocation(FVector(0, 0, 0));
-	meshComponent->SetWorldScale3D(FVector(1, 1, 1));
+	meshComponent->SetWorldScale3D(FVector(0.4, 0.4, 0.4));
 	meshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 
 	UStaticMesh* obj = Cast<UStaticMesh>(this->requestHandle->GetLoadedAsset());
 	//GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Green, "debug");
 	if (obj) {
 		meshComponent->SetStaticMesh(obj);
-		meshComponent->SetWorldScale3D(FVector(1, 1, 1));
+		meshComponent->SetWorldScale3D(FVector(0.4, 0.4, 0.4));
 		meshComponent->SetVisibility(true, true);
 	}
 	else {
